d62f3bb20b4050aee78ee3c7649e830575d9b829
Nilanjan Roy <realnilanjan@gmail.com>
2020-10-13 12:36:42 +0530

    Added Business and User Table

---
+++ PointOfSale.Db/dbo/Tables/User.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[UserId] INT NOT NULL,
+    [Fullname] NVARCHAR (50)  NULL,
+    [Username] NVARCHAR (50) NULL,
+    [Password] NVARCHAR (100) NULL,
+    [UserRole] NVARCHAR (50)  NULL,
+    [Contact] NVARCHAR (10) NULL,
+    [EmailAddress] NVARCHAR (100)  NULL,
+    [Status] NVARCHAR (50)  NULL,
+    CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([UserId] ASC)
+)
