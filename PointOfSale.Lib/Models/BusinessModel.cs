d245a1962658d309bafaf8d39e3092b160579646
Nilanjan Roy <realnilanjan@gmail.com>
2020-10-13 12:37:16 +0530

    Tweaked some property names

---
+++ PointOfSale.Lib/Models/BusinessModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointOfSale.Lib.Models
+{
+    public class BusinessModel
+    {
+        public string BusinessName { get; set; }
+        public string GSTNumber { get; set; }
+        public string BusinessType { get; set; }
+        public string BusinsessAddress { get; set; }
+        public string BusinessContact { get; set; }
+        public string BusinessEmail { get; set; }
+    }
+}
