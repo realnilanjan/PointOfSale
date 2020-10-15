de0452afb54b563ed45637d58d7f4f29a664503b
Nilanjan Roy <realnilanjan@gmail.com>
2020-10-15 23:37:00 +0530

    Added Stored Procedures

---
+++ PointOfSale.Db/dbo/Stored Procedures/LoadAllUsers.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[LoadAllUsers]
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SELECT [UserId], [Fullname], [Username], [Password], [UserRole], [Contact], [EmailAddress], [Status]
+	FROM [dbo].[User];
+END
