a8aa5157edb4bb3d3525ceff3c7fcf2ae1d01008
Nilanjan Roy <realnilanjan@gmail.com>
2020-10-15 23:37:55 +0530

    Added MD5 Hashing

---
+++ PointOfSale.Lib/Encryptions/EncryptTo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointOfSale.Lib.Encryptions
+{
+    public class EncryptTo
+    {
+        public static string MD5Hash(string value)
+        {
+            MD5CryptoServiceProvider MD5Code = new MD5CryptoServiceProvider();
+            byte[] diziByte = Encoding.UTF8.GetBytes(value);
+            diziByte = MD5Code.ComputeHash(diziByte);
+            StringBuilder sb = new StringBuilder();
+            foreach (byte ba in diziByte)
+            {
+                sb.Append(ba.ToString("x2").ToUpper());
+            }
+            return sb.ToString();
+        }
+    }
+}
