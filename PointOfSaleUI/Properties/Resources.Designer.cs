f6752b1c355d34e1dd0a360db514adf33fc17530
Nilanjan Roy <realnilanjan@gmail.com>
2020-10-16 11:40:25 +0530

    Dummy Resource

--- PointOfSaleUI/Properties/Resources.Designer.cs
+++ PointOfSaleUI/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace PointOfSaleUI.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap dashboard_png {
+            get {
+                object obj = ResourceManager.GetObject("dashboard.png", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap female {
             get {
                 object obj = ResourceManager.GetObject("female", resourceCulture);
