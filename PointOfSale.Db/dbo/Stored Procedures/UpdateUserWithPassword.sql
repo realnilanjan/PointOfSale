7fa2cef774e4abccf67327d2f6c6f56c16c68d0a
Nilanjan Roy <realnilanjan@gmail.com>
2020-10-16 20:45:23 +0530

    Added Delete Update Users Procedures

---
+++ PointOfSale.Db/dbo/Stored Procedures/UpdateUserWithPassword.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[UpdateUserWithPassword]
+	@UserId int,
+	@Fullname nvarchar(50),
+	@Username nvarchar(50),
+	@Password nvarchar(100),
+	@UserRole nvarchar(50),
+	@Contact nvarchar(10),
+	@EmailAddress nvarchar(100)
+AS
+BEGIN
+	SET NOCOUNT ON;
+	UPDATE [dbo].[User]
+	SET [Fullname]=@Fullname, [Username]=@Username, [Password]=@Password, [UserRole]=@UserRole, [Contact]=@Contact, [EmailAddress]=@EmailAddress
+	WHERE UserId = @UserId;
+END
