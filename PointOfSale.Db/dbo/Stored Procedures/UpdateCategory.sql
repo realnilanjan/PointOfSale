64fe271f778bb3eeb0101e5759147e0d0b3e4ff8
Nilanjan Roy <realnilanjan@gmail.com>
2020-10-17 22:45:05 +0530

    Added Procedures for Categories

---
+++ PointOfSale.Db/dbo/Stored Procedures/UpdateCategory.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[UpdateCategory]
+	@Id int,
+	@CategoryName nvarchar(50),
+	@CategoryDescription nvarchar(100)
+AS
+BEGIN
+	SET NOCOUNT ON;
+	UPDATE [dbo].[Category]
+	SET CategoryName=@CategoryName, CategoryDescription=@CategoryDescription
+	WHERE Id=@Id;
+END
