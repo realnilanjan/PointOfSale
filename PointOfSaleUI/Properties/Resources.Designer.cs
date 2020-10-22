c7309a57612117f64d7f6e1aec0d49b48e59a8d5
Nilanjan Roy <realnilanjan@gmail.com>
2020-10-22 22:07:02 +0530

    Added some images to resources and tweaked some UI

--- PointOfSaleUI/Properties/Resources.Designer.cs
+++ PointOfSaleUI/Properties/Resources.Designer.cs
@@ -83,6 +83,16 @@ namespace PointOfSaleUI.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap clear {
+            get {
+                object obj = ResourceManager.GetObject("clear", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap close {
             get {
                 object obj = ResourceManager.GetObject("close", resourceCulture);
