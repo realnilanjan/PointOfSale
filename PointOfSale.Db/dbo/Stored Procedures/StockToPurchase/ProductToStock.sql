c83f78a75dd79fa6f0f6062d2e78e04e7fcedf59
Nilanjan Roy <realnilanjan@gmail.com>
2020-10-25 02:09:19 +0530

    Fixed Bug

--- PointOfSale.Db/dbo/Stored Procedures/StockToPurchase/ProductToStock.sql
+++ PointOfSale.Db/dbo/Stored Procedures/StockToPurchase/ProductToStock.sql
@@ -3,7 +3,7 @@
 AS
 BEGIN
 	SET NOCOUNT ON;
-	SELECT [Brand], [Name], [StockPrice], [CategoryId], [SupplierId]
+	SELECT [Brand], [Name], [StockPrice], [SupplierId]
 	FROM Products  
 	WHERE Barcode = @Barcode
 END
