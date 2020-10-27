b59ae37a19f8685c0345b640b1888d64187000cc
Nilanjan Roy <realnilanjan@gmail.com>
2020-10-27 18:03:20 +0530

    Added grid coloring to teh UI

---
+++ PointOfSaleUI/Helpers/Functions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PointOfSaleUI.Helpers
+{
+    public static class Functions
+    {
+        public static void ProcessGridColor(DataGridView grid, int cellvalue)
+        {
+            foreach (DataGridViewColumn c in grid.Columns)
+            {
+                c.SortMode = DataGridViewColumnSortMode.Programmatic;
+            }
+
+            foreach (DataGridViewRow rw in grid.Rows)
+            {
+                if (Convert.ToInt32(rw.Cells[cellvalue].Value) <= 5)
+                {
+                    rw.DefaultCellStyle.BackColor = Properties.Settings.Default.ZeroToFiveBackColor;
+                    rw.DefaultCellStyle.SelectionBackColor = Properties.Settings.Default.ZeroToFiveRowSelectionBackColor;
+                    rw.DefaultCellStyle.SelectionForeColor = Properties.Settings.Default.ZeroToFiveRowRowSelectionForeColor;
+                    rw.DefaultCellStyle.ForeColor = Properties.Settings.Default.ZeroToFiveRowSelectionForeColor;
+                }
+                else if (Convert.ToInt32(rw.Cells[cellvalue].Value) > 5 && Convert.ToInt32(rw.Cells[cellvalue].Value) <= 10)
+                {
+                    rw.DefaultCellStyle.BackColor = Properties.Settings.Default.FiveToTenBackColor;
+                    rw.DefaultCellStyle.SelectionBackColor = Properties.Settings.Default.FiveToTenRowSelectionBackColor;
+                    rw.DefaultCellStyle.SelectionForeColor = Properties.Settings.Default.FiveToTenRowRowSelectionForeColor;
+                    rw.DefaultCellStyle.ForeColor = Properties.Settings.Default.FiveToTenRowSelectionForeColor;
+                }
+
+                else if (Convert.ToInt32(rw.Cells[cellvalue].Value) > 10 && Convert.ToInt32(rw.Cells[cellvalue].Value) <= 15)
+                {
+                    rw.DefaultCellStyle.BackColor = Properties.Settings.Default.TenToFifteenBackColor;
+                    rw.DefaultCellStyle.SelectionBackColor = Properties.Settings.Default.TenToFifteenRowSelectionBackColor;
+                    rw.DefaultCellStyle.SelectionForeColor = Properties.Settings.Default.TenToFifteenRowRowSelectionForeColor;
+                    rw.DefaultCellStyle.ForeColor = Properties.Settings.Default.TenToFifteenRowSelectionForeColor;
+                }
+            }
+        }
+    }
+}
+    
