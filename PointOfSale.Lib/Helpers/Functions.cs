004007bcdb2d741367697849400413041eae1f51
Nilanjan Roy <realnilanjan@gmail.com>
2020-10-27 18:02:41 +0530

    Moved grif coloring to UI

--- PointOfSale.Lib/Helpers/Functions.cs
+++ PointOfSale.Lib/Helpers/Functions.cs
@@ -28,38 +28,5 @@ namespace PointOfSale.Lib.Helpers
             control.ValueMember = "Id";
             control.DisplayMember = "QtyDescription";
         }
-
-        public static void ProcessGridColor(DataGridView grid, int cellvalue)
-        {
-            foreach (DataGridViewColumn c in grid.Columns)
-            {
-                c.SortMode = DataGridViewColumnSortMode.Programmatic;
-            }
-
-            foreach (DataGridViewRow rw in grid.Rows)
-            {
-                if (Convert.ToInt32(rw.Cells[cellvalue].Value) <= 5)
-                {
-                    rw.DefaultCellStyle.BackColor = Color.Red;
-                    rw.DefaultCellStyle.SelectionBackColor = Color.FromName("Highlight");
-                    rw.DefaultCellStyle.SelectionForeColor = Color.White;
-                    rw.DefaultCellStyle.ForeColor = Color.White;
-                }
-                else if (Convert.ToInt32(rw.Cells[cellvalue].Value) > 5 && Convert.ToInt32(rw.Cells[cellvalue].Value) <= 10)
-                {
-                    rw.DefaultCellStyle.BackColor = Color.LightCoral;
-                    rw.DefaultCellStyle.SelectionBackColor = Color.FromName("Highlight");
-                    rw.DefaultCellStyle.SelectionForeColor = Color.Black;
-                    rw.DefaultCellStyle.ForeColor = Color.Black;
-                }
-                else if (Convert.ToInt32(rw.Cells[cellvalue].Value) > 10 && Convert.ToInt32(rw.Cells[cellvalue].Value) <= 15)
-                {
-                    rw.DefaultCellStyle.BackColor = Color.FromArgb(255, 255, 128);
-                    rw.DefaultCellStyle.SelectionBackColor = Color.FromName("Highlight");
-                    rw.DefaultCellStyle.SelectionForeColor = Color.Black;
-                    rw.DefaultCellStyle.ForeColor = Color.Black;
-                }
-            }
-        }
     }
 }
