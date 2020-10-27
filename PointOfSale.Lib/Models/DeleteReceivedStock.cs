8152348cb4fcaa9bff6345a1efbdee4fc2c21219
Nilanjan Roy <realnilanjan@gmail.com>
2020-10-27 18:03:06 +0530

    Added functionaliyu to delete received stock

---
+++ PointOfSale.Lib/Models/DeleteReceivedStock.cs
@@ -0,0 +1,8 @@
+﻿namespace PointOfSale.Lib.Models
+{
+    public class DeleteReceivedStock
+    {
+        public string Barcode { get; set; }
+        public int ReceivedQty { get; set; }
+    }
+}
