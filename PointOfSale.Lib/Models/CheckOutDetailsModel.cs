6df0e89ebd4ef3c8e0554ab96bcab62e3630f340
Nilanjan Roy <realnilanjan@gmail.com>
2020-10-30 01:28:25 +0530

    Added models for saving checkout

---
+++ PointOfSale.Lib/Models/CheckOutDetailsModel.cs
@@ -0,0 +1,13 @@
+﻿namespace PointOfSale.Lib.Models
+{
+    public class CheckOutDetailsModel
+    {
+        public int CashierId { get; set; }
+        public string InvoiceNumber { get; set; }
+        public decimal SubTotal { get; set; }
+        public int CouponId { get; set; }
+        public decimal SaleTaxRate { get; set; }
+        public decimal ShippingRate { get; set; }
+        public decimal GrandTotal { get; set; }
+    }
+}
---
+++ PointOfSale.Lib/Models/CheckOutModel.cs
@@ -0,0 +1,20 @@
+﻿using PointOfSale.Lib.TerminalModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointOfSale.Lib.Models
+{
+    public class CheckOutModel
+    {
+        public int CashierId { get; set; }
+        public string InvoiceNumber { get; set; }
+        public decimal SubTotal { get; set; }
+        public int CouponId { get; set; }
+        public decimal SaleTaxRate { get; set; }
+        public decimal ShippingRate { get; set; }
+        public decimal GrandTotal { get; set; }
+    }
+}
