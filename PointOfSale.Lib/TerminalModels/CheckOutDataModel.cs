306dc3f059490ee909876f0b97a3e710dc10ff58
Nilanjan Roy <realnilanjan@gmail.com>
2020-11-01 22:44:50 +0530

    Modified functionality of Transaction

--- PointOfSale.Lib/TerminalModels/CheckOutDataModel.cs
+++ PointOfSale.Lib/TerminalModels/CheckOutDataModel.cs
@@ -1,18 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PointOfSale.Lib.TerminalModels
 {
-    public class CheckOutDataModel
+    public static class CheckOutDataModel
     {
-        public int SaleId { get; set; }
-        public int CashierId { get; set; }
-        public decimal TotalAmount { get; set; }
-        public int CardDigits { get; set; }
-        public string CardType { get; set; }
-        public DateTime DateCreated { get; set; } = DateTime.UtcNow.AddHours(5.5);
+        public static int SaleId { get; set; }
+        public static string CardDigits { get; set; }
+        public static string CardType { get; set; }
     }
 }
---
+++ PointOfSale.Lib/TerminalModels/CheckOutModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PointOfSale.Lib.TerminalModels
+{
+    public class CheckOutModel
+    {
+        public int SaleId { get; set; }
+        public int CashierId { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string CardDigits { get; set; }
+        public string CardType { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
