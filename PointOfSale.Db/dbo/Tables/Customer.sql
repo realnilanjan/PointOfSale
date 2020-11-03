9d56560be517aa9a6a29c6742eaf9ba08cd54056
Nilanjan Roy <realnilanjan@gmail.com>
2020-11-03 12:14:26 +0530

    Added Customer Table and added procedures to get all customers

---
+++ PointOfSale.Db/dbo/Tables/Customer.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Customer]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [CustomerName] NVARCHAR(50) NOT NULL, 
+    [CustomerEmail] NVARCHAR(100) NOT NULL, 
+    [CustomerPhone] NVARCHAR(10) NOT NULL, 
+    [CustomerAddressLine1] NVARCHAR(100) NOT NULL, 
+    [CustomerAddressLine2] NVARCHAR(100) NULL, 
+    [CustomerAddressPin] NVARCHAR(10) NOT NULL, 
+    [RegistrationDate] DATETIME2 NOT NULL
+)
