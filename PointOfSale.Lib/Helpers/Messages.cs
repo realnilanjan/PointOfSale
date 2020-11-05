e2f31b4e0b87bc9acb652e5845b80650b468391b
Nilanjan Roy <realnilanjan@gmail.com>
2020-11-05 10:37:42 +0530

    Fixed bugs for error thrown at incompletion of timer event

--- PointOfSale.Lib/Helpers/Messages.cs
+++ PointOfSale.Lib/Helpers/Messages.cs
@@ -8,18 +8,31 @@ namespace PointOfSale.Lib.Helpers
     {
         public static void DisplayMessage(String message, Label label, Color color,int Interval = 3000)
         {
-            System.Timers.Timer timer = new System.Timers.Timer();
-            timer.Interval = Interval;
-            label.Invoke(new Action(() => label.Text = message));
-            label.Invoke(new Action(() => label.Visible = true));
-            label.Invoke(new Action(() => label.BackColor = color));
-            // above two line sets the visibility and shows the message and interval elapses hide the visibility of the label. Elapsed will we called after Start() method.
-
-            timer.Elapsed += (s, en) => {
-                label.Invoke(new Action(() => label.Visible = false));
-                timer.Stop(); // Stop the timer(otherwise keeps on calling)
-            };
-            timer.Start(); // Starts the timer. 
+            try
+            {
+                System.Timers.Timer timer = new System.Timers.Timer();
+                timer.Interval = Interval;
+                label.Invoke(new Action(() => label.Text = message));
+                label.Invoke(new Action(() => label.Visible = true));
+                label.Invoke(new Action(() => label.BackColor = color));
+                timer.Elapsed += (s, en) =>
+                {
+                    try
+                    {
+                        label.Invoke(new Action(() => label.Visible = false));
+                        timer.Stop();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                };
+                timer.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
