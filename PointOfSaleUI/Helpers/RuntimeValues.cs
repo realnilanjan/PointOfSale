1e17a44312d2965bf8de0a2bd104489ecbc36216
Nilanjan Roy <realnilanjan@gmail.com>
2020-11-09 23:56:57 +0530

    Added function to remove saved transaction file after checkout

---
+++ PointOfSaleUI/Helpers/RuntimeValues.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Reflection;
+
+namespace PointOfSaleUI.Helpers
+{
+    public static class RuntimeValues
+    {
+        public static string DraftFileName { get; set; }
+    }
+}
