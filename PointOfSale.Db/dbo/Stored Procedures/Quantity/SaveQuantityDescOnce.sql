c37f05ff529fb174fcb1cd452623c48db572cb2e
Nilanjan Roy <realnilanjan@gmail.com>
2020-11-19 18:03:10 +0530

    Added once data populate in Quantity Description

---
+++ PointOfSale.Db/dbo/Stored Procedures/Quantity/SaveQuantityDescOnce.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[SaveQuantityDescOnce]
+	@QtyDescription nvarchar(50)
+AS
+BEGIN
+	SET NOCOUNT ON;
+	INSERT INTO [QuantityDescription] (QtyDescription)
+	VALUES (@QtyDescription)
+END
